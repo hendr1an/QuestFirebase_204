fab97d02ac5f4069d2d74d83cbb762dcc9705cec
hendr1an <hendriansyahgyan@gmail.com>
2026-01-03 05:43:34 +0700

    konfigurasi MainActivity.kt

--- app/src/main/java/com/example/MyFirebase/MainActivity.kt
+++ app/src/main/java/com/example/MyFirebase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.questfirebase_204
+package com.example.myfirebase
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,41 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.questfirebase_204.ui.theme.QuestFirebase_204Theme
+import com.example.myfirebase.ui.theme.MyFirebaseTheme
+import com.example.myfirebase.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuestFirebase_204Theme {
+            MyFirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestFirebase_204Theme {
-        Greeting("Android")
     }
 }
