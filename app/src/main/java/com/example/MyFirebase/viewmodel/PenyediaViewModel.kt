d676e5c99894e16f939beb59c79e344994ed565f
hendr1an <hendriansyahgyan@gmail.com>
2026-01-03 05:43:56 +0700

    konfigurasi PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/MyFirebase/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myfirebase.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel(createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel(createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
