81bdaee319d18b6f3d0ecbf3d768d44407a70c08
hendr1an <hendriansyahgyan@gmail.com>
2026-01-03 05:39:02 +0700

    konfigurasi build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services) // Penting untuk Firebase
 }
 
 android {
-    namespace = "com.example.questfirebase_204"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.example.myfirebase"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.questfirebase_204"
-        minSdk = 29
+        applicationId = "com.example.myfirebase"
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -50,6 +49,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Firebase & Navigation (Sesuai dengan alias di TOML)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.navigation.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.coroutines.play.services)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
