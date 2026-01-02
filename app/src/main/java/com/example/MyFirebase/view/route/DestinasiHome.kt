7f11c2abcd6e6918e0b8f1c23a492fd2c6e39281
hendr1an <hendriansyahgyan@gmail.com>
2026-01-03 05:40:15 +0700

    konfigurasi DestinasiHome.kt

---
+++ app/src/main/java/com/example/MyFirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,6 @@
+package com.example.myfirebase.view.route
+import com.example.myfirebase.R
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
