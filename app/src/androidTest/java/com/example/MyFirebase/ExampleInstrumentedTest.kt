a8826f651e8a2bcc6f787e65387217a408cb49f3
hendr1an <hendriansyahgyan@gmail.com>
2026-01-03 05:42:08 +0700

    konfigurasi ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/MyFirebase/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/MyFirebase/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.questfirebase_204
+package com.example.MyFirebase
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
